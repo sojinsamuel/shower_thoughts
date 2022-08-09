58200986b3eb57ab69dfe0eebff2dd1bdbc14a57
James Neff <james.m.neff@gmail.com>
2022-08-09 12:17:48 -0400

    connect database

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //load config
 dotenv.config({ path: './config/config.env'})
 
+connectDB();
+
+
 const app = express();
+
+//logging
+if(process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
