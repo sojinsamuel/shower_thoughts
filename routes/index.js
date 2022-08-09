b1cfd98560081fe525aebd028e94c7597bb0fb86
James Neff <james.m.neff@gmail.com>
2022-08-09 14:16:43 -0400

    setup auth middleware and protective routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 /////////////////////////////
 // @descrip   Login / Landing Page
 // @route     GET /
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
   res.render('login', {
     layout: 'login'
   })
@@ -13,7 +14,7 @@ router.get('/', (req, res) => {
 /////////////////////////////
 // @descrip   Dashboard
 // @route     GET /dash
-router.get('/dash', (req, res) => {
+router.get('/dash', ensureAuth, (req, res) => {
   res.render('dashboard')
 })
 
