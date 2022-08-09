81d624628b1d0014d69e2dd80d6749da1331e0a4
James Neff <james.m.neff@gmail.com>
2022-08-09 12:43:46 -0400

    intial routes and views

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+
+/////////////////////////////
+// @descrip   Login / Landing Page
+// @route     GET /
+router.get('/', (req, res) => {
+  res.render('login')
+})
+
+/////////////////////////////
+// @descrip   Dashboard
+// @route     GET /dash
+router.get('/dash', (req, res) => {
+  res.render('dashboard')
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 //load config
@@ -15,5 +16,14 @@ const app = express();
 if(process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 5000
+
+
+//handlebars
+app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs')
+app.set('views', './views');
+
+//routes
+app.use('/', require('./routes/index'))
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
