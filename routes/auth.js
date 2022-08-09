93aa45e733a4254d6665c800417892548167584e
James Neff <james.m.neff@gmail.com>
2022-08-09 14:11:48 -0400

    setup user login and logout

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+/////////////////////////////
+// @descrip   Auth with google
+// @route     GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+/////////////////////////////
+// @descrip   google auth callback
+// @route     GET /auth/google/callback
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dash')
+  }
+)
+
+/////////////////////////////
+// @descrip   logout user
+// @route     GET /auth/logout
+router.get('/logout', (req, res) => {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 //load config
 dotenv.config({ path: './config/config.env'})
+//passport config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -21,14 +25,25 @@ const PORT = process.env.PORT || 5000
 
 //handlebars
 app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs')
+app.set('view engine', '.hbs');
 app.set('views', './views');
 
+//express session middleware
+app.use(session({
+  secret: 'penny thoughts',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
